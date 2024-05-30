39d4dcafa84a1b828f94ef3e67ee6134fb2d2781
Christian Montoya <christian.montoya@consensys.net>
2024-05-30 18:37:34 -0400

    init

--- packages/snap/README.md
+++ packages/snap/README.md
@@ -1,12 +1,5 @@
-# TypeScript Example Snap
+# Tx Bookmark 
 
-This snap demonstrates how to develop a snap with TypeScript. It is a simple
-snap that displays a confirmation dialog when the `hello` JSON-RPC method is
-called.
+A Snap for tracking your transactions...
 
-## Testing
-
-The snap comes with some basic tests, to demonstrate how to write tests for
-snaps. To test the snap, run `yarn test` in this directory. This will use
-[`@metamask/snaps-jest`](https://github.com/MetaMask/snaps/tree/main/packages/snaps-jest)
-to run the tests in `src/index.test.ts`.
+Icon is: bookmark by NeueDeutsche from Noun Project (CC BY 3.0)
--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,36 +1,79 @@
-import type { OnRpcRequestHandler } from '@metamask/snaps-sdk';
-import { panel, text } from '@metamask/snaps-sdk';
+import type { OnTransactionHandler, OnHomePageHandler } from '@metamask/snaps-sdk';
+import { panel, text, copyable, row, divider, address, heading } from '@metamask/snaps-sdk';
 
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
- */
-export const onRpcRequest: OnRpcRequestHandler = async ({
-  origin,
-  request,
+export type TransactionState = { 
+  transactions: Array<any>,
+  bookmarks: Array<any>
+}; 
+
+export const onTransaction: OnTransactionHandler = async ({
+  transaction,
+  chainId,
+  transactionOrigin,
 }) => {
-  switch (request.method) {
-    case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'confirmation',
-          content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
-          ]),
-        },
-      });
-    default:
-      throw new Error('Method not found.');
+
+  let data:TransactionState = await snap.request({
+    method: "snap_manageState",
+    params: { operation: "get" },
+  }) as TransactionState || { transactions: [], bookmarks: [] };
+
+  while(data.transactions.length > 9) { // magic number 
+    data.transactions.pop(); 
   }
+
+  data.transactions.unshift({
+    transaction: transaction,
+    chainId: chainId, 
+    transactionOrigin: transactionOrigin
+  }); 
+
+  await snap.request({ 
+    method: "snap_manageState",
+    params: { operation: "update", newState: data }
+  }); 
+
+  let rows = []; 
+  rows.push(row("Origin", text(`${transactionOrigin}`))); 
+  rows.push(row("Chain ID", text(`${chainId}`))); 
+  rows.push(row("From",address(`${transaction.from}`))); 
+  if(transaction.to) rows.push(row("To",address(`${transaction.to}`))); 
+
+  return {
+    content: panel([
+      heading("Transaction"),
+      ...rows,
+      text("Transaction data:"),
+      copyable(JSON.stringify(transaction)),
+    ]),
+  };
+};
+
+export const onHomePage: OnHomePageHandler = async () => {
+  const data:TransactionState = await snap.request({
+    method: "snap_manageState",
+    params: { operation: "get" },
+  }) as TransactionState || { transactions: [], bookmarks: [] };
+  if(data.transactions.length < 1) { 
+    return { 
+      content: panel([
+        text("You have not initiated any transactions since installing this Snap. Your recent transaction requests will show up here.")
+      ])
+    }; 
+  }
+  let list = []; 
+  for(let i = 0; i < data.transactions.length; i++) { 
+    const tx = data.transactions[i]; 
+    list.push(divider()); 
+    list.push(row("Origin",text(tx.transactionOrigin))); 
+    list.push(row("Chain ID",text(tx.chainId))); 
+    list.push(row("From",address(`${tx.transaction.from}`))); 
+    if(tx.transaction.to) list.push(row("To",address(`${tx.transaction.to}`))); 
+    list.push(copyable(JSON.stringify(tx.transaction))); 
+  }
+  return { 
+    content: panel([
+      text(`Last ${data.transactions.length} transactions:`),
+      ...list
+    ])
+  }; 
 };
